05d727285f6e9023b6f3e5933bd565494f0c4bd5
dimtaiwo <dimtaiwo@gmail.com>
2025-12-24 15:01:05 +0000

    changed test scripts

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (
+      typeof args[0] === "string" &&
+      args[0].includes("ReactDOMTestUtils.act")
+    ) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
